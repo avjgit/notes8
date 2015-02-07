14ae6e17c8a7414c5e1c204d5ca7462a10f1ef84
AJ <avjmail@gmail.com>
2015-02-07 20:52:22 +0200

    sample app init

---
+++ aspnetmvc_mvaintro/aspnetmvc_mvaintro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnetmvc_mvaintro.Startup))]
+namespace aspnetmvc_mvaintro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
